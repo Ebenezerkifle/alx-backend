104977c92eba46f31cf56e73662c9bcf2bc60d00
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2024-01-30 16:26:18 +0300

    cache class is added

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" A subclass of Base Caching module
+"""
+from base_caching import BaseCaching
+
+class BasicCache(BaseCaching):
+    """ Class that inherits from BaseCaching and is a caching system
+        This caching system doesn’t have limit """
+    
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        """ Assign to the dictionary """
+        if key is None or item is None:
+            return
+        self.cache_data.put(key, item)
+
+    def get(self, key):
+        if key is None:
+            return None
+        if self.cache_data.__contains__(key):
+            return self.cache_data.get(key)
+        else:
+            return None
