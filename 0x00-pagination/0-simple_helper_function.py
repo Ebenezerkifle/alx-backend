2eb3f130c16e75f0289d0c3e7730c06206634f49
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2024-01-30 05:13:21 +0300

    A function to return a tuples of size to that contains the pagination information

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -7,6 +7,6 @@ def index_range(page, page_size):
     to the range of indexes to return in a list for 
     those particular pagination parameters."""
     if page and page_size:
-        start_index = (page -1)* page_size
+        start_index = (page -1) * page_size
         end_index = start_index + page_size
         return start_index, end_index
