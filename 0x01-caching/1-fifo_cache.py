6e79ad4455c600c0ba79f42b1dbc728b64293e87
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2024-01-30 16:56:26 +0300

    FIFO caching added

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,26 +1,47 @@
 #!/usr/bin/python3
-"""FIFOCache class that inherits from BaseCaching class
+""" FIFO Cache class that inherits from BaseCaching class
 """
 
 from base_caching import BaseCaching
 
 class FIFOCache(BaseCaching):
-    """"""
+    """ Class that inherits from BaseCaching and is a caching system """
     
     def __init__(self):
         super().__init__()
+        self.data = {}
+        self.next_in, self.next_out = 0, 0
     
     def put(self, key, item):
-        if key is None or item is None:
-            return
-        self.cache_data[key] = item
+        if key and item:
+            if key in self.cache_data:
+                self.cache_data[key] = item
+            else:
+                self._push(key, item)
         """Checking if the items number reachs Max item number"""
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
             # Discard the first item FIFO
             print('DISCARD: ', self.cache_data[0].key, '\n')
     
     def get(self, key):
-        if key is None:
+        """ Return the value linked """
+        if key is None or self.cache_data.get(key) is None:
             return None
-        return self.cache_data.get(key)
+        if key in self.cache_data:
+            value = self.cache_data[key]
+            return value
 
+    def _push(self, key, item):
+        """ FIFO algorithm, add element """ 
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS - 1:
+            print("DISCARD: {}".format(self.data[self.next_out + 1]))
+            self._pop()
+        self.cache_data[key] = item
+        self.next_in += 1
+        self.data[self.next_in] = key
+
+    def _pop(self):
+        """FIFO algorithm to remove an element"""
+        self.next_out += 1
+        key = self.data[self.next_out]
+        del self.data[self.next_out], self.cache_data[key]
