3fb11734e1430d18fbbc58e9cf27ec4d0065e342
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2024-01-30 05:28:14 +0300

    getting a data from local file using pagination is done

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
-""" Simple helper function """
-
-
+""" Simple pagination """
 import csv
 import math
 from typing import List
 
-def index_range(self, page: int, page_size: int):
+
+def index_range(page, page_size):
+    """ return a tuple of size two containing a start index and an end index
+        corresponding to the range of indexes to return in a list for those
+        particular pagination parameters. """
     if page and page_size:
         start_index = (page - 1) * page_size
         end_index = start_index + page_size
@@ -34,9 +36,9 @@ class Server:
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """ obtains the indexes and return corresponding pages """
-        assert type(page).isinstance(int) and page > 0
-        assert type(page_size).isinstance(int) and page_size > 0
-        start, end = self.index_range(page, page_size)
+        assert type(page) == int and page > 0
+        assert type(page_size) == int and page_size > 0
+        start, end = index_range(page, page_size)
         pages = []
         if start >= len(self.dataset()):
             return pages
