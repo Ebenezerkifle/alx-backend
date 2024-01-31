c6e44206256e2925c27f10fd1572a252e73f0c1d
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2024-01-31 04:29:54 +0300

    LIFO cache is added

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -4,22 +4,22 @@ from base_caching import BaseCaching
 
 class LIFOCache(BaseCaching):
     """A caching class to handle caching using LIFO structure"""
-
     def __init__(self):
         super().__init__()
         self.last_key = ''
 
     def put(self, key, item):
+        """ Assign to the dictionary, LIFO algorithm, add element """
         if key and item:
             # Assigning to the dictionary
             self.cache_data[key] = item
             if len(self.cache_data) > BaseCaching.MAX_ITEMS:
                 # Let's remove the item
-                print('DISCARD: {}'.format(self.last_key))
+                print("DISCARD: {}".format(self.last_key))
                 self.cache_data.pop(self.last_key)
             self.last_key = key
     
     def get(self, key):
-        if key is None or self.cache_data.get(key) is None:
-            return None
-        return self.cache_data.get(key)
+        if key and key in self.cache_data:
+            return self.cache_data.get(key)
+        return None
